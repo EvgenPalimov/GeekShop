e5663b9ee302a2f0c490154cf2f2004b3c83d986
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2021-11-20 23:10:29 +0500

    initionall authapp

---
+++ authapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+# from mainapp.views import registration, login
+
+app_name = 'authapp'
+urlpatterns = [
+    # path('registration/', registration, name='registration'),
+    # path('login/', login, name='login'),
+]
+
+
