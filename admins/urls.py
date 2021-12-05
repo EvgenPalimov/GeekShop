1cc02503be46a36b19585f115a844a40a0f486a7
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2021-12-06 00:41:06 +0500

    Lesson_8_admin_users_on_CBV

--- admins/urls.py
+++ admins/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
 
-from admins.views import index, admin_users, admin_users_create, admin_users_update, admin_users_delete, \
+from admins.views import UserTemplateView, UserListView, UserCreateView, UserUpdateView, UserDeleteView, \
     admin_products, admin_products_create, admin_products_update, admin_products_delete, admin_categories, \
     admin_categories_create, admin_categories_update, admin_categories_delete
 
 app_name = 'admins'
 urlpatterns = [
-    path('', index, name='index'),
-    path('users/', admin_users, name='admin_users'),
-    path('users-create/', admin_users_create, name='admin_users_create'),
-    path('users-update/<int:pk>', admin_users_update, name='admin_users_update'),
-    path('users-delete/<int:pk>', admin_users_delete, name='admin_users_delete'),
+    path('', UserTemplateView.as_view(), name='index'),
+    path('users/', UserListView.as_view(), name='admin_users'),
+    path('users-create/', UserCreateView.as_view(), name='admin_users_create'),
+    path('users-update/<int:pk>', UserUpdateView.as_view(), name='admin_users_update'),
+    path('users-delete/<int:pk>', UserDeleteView.as_view(), name='admin_users_delete'),
     path('products/', admin_products, name='admin_products'),
     path('products-create/', admin_products_create, name='admin_products_create'),
     path('products-update/<int:pk>', admin_products_update, name='admin_products_update'),
--- admins/views.py
+++ admins/views.py
@@ -3,75 +3,59 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
 
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm, ProductAdminRegistrationForm, \
     ProductAdminProfileForm, ProductCategoriesAdminRegistrationForm, ProductCategoriesAdminProfileForm
 from authapp.models import User
+from mainapp.mixin import CustomDispatchMixin, BaseClassContextMixin
 from mainapp.models import Product, ProductCategory
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def index(request):
-    return render(request, 'admins/admin.html')
+class UserTemplateView(TemplateView, CustomDispatchMixin):
+    model = User
+    template_name = 'admins/admin.html'
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users(request):
-    context = {
-        'title': 'GeekShop - Админ',
-        'users': User.objects.all()
-    }
-    return render(request, 'admins/admin-users-read.html', context)
+class UserListView(ListView, CustomDispatchMixin, BaseClassContextMixin):
+    model = User
+    template_name = 'admins/admin-users-read.html'
+    title = 'Админ | Пользователи'
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users_create(request):
-    if request.method == 'POST':
-        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    else:
-        form = UserAdminRegistrationForm
-    context = {
-        'title': 'GeekShop - Админ | Регистрация',
-        'form': form
-    }
-    return render(request, 'admins/admin-users-create.html', context)
+class UserCreateView(CreateView, CustomDispatchMixin, BaseClassContextMixin):
+    model = User
+    template_name = 'admins/admin-users-create.html'
+    form_class = UserAdminRegistrationForm
+    success_url = reverse_lazy('admins:admin_users')
+    title = 'Админ | Создать пользователя'
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users_update(request, pk):
-    user_select = User.objects.get(pk=pk)
-
-    if request.method == 'POST':
-        form = UserAdminProfileForm(data=request.POST, instance=user_select, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect(reverse('admins:admin_users'))
-    else:
-        form = UserAdminRegistrationForm(instance=user_select)
-    context = {
-        'title': 'GeekShop - Админ | Редактирование',
-        'form': form,
-        'user_select': user_select
-    }
-    return render(request, 'admins/admin-users-update-delete.html', context)
+class UserUpdateView(UpdateView, CustomDispatchMixin, BaseClassContextMixin):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    form_class = UserAdminProfileForm
+    success_url = reverse_lazy('admins:admin_users')
+    title = 'Админ | Редактирование пользователя'
 
 
-@user_passes_test(lambda u: u.is_superuser)
-def admin_users_delete(request, pk):
-    if request.method == 'POST':
-        user = User.objects.get(pk=pk)
-        if user.is_active == True:
-            user.is_active = False
-            user.save()
+class UserDeleteView(DeleteView, CustomDispatchMixin, BaseClassContextMixin):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    form_class = UserAdminProfileForm
+    success_url = reverse_lazy('admins:admin_users')
+    title = 'Админ | Удаление пользователя'
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.is_active:
+            self.object.is_active = False
         else:
-            user.is_active = True
-            user.save()
-
-    return HttpResponseRedirect(reverse('admins:admin_users'))
+            self.object.is_active = True
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
 
 @user_passes_test(lambda u: u.is_superuser)
