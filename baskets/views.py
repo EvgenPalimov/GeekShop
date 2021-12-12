f60ec28bad4c3539ded2ede168ddb49d7dfedc1a
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2021-12-12 20:02:32 +0500

    Finally project

--- baskets/views.py
+++ baskets/views.py
@@ -1,53 +1,46 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, JsonResponse
 # Create your views here.
-from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
-from django.views.generic import CreateView
 
 from baskets.models import Basket
-from mainapp.mixin import UserDipatchMixin
 from mainapp.models import Product
-
-class BasketAddCreateView(CreateView, UserDipatchMixin):
-    title = 'GeekShop | Создать продукт'
-
-
-    def post(self, request, *args, **kwargs):
-        if request.is_ajax:
-            user_select = request.user
-            product = Product.objects.get(id=self.kwargs.get('id'))
-            baskets = Basket.objects.filter(user=user_select, product=product)
-            if baskets:
-                basket = baskets.first()
-                basket.quantity += 1
-                basket.save()
-            else:
-                Basket.objects.create(user=user_select, product=product, quantity=1)
-
-            products = Product.objects.all()
-            context = {'products': products}
-            result = render_to_string('mainapp/includes/card.html', context, request=request)
-            return JsonResponse({'result': result})
 
 
 # @login_required
-# def basket_add(request, id):
-#     if request.is_ajax():
-#         user_select = request.user
-#         product = Product.objects.get(id=id)
-#         baskets = Basket.objects.filter(user=user_select, product=product)
-#         if baskets:
-#             basket = baskets.first()
-#             basket.quantity += 1
-#             basket.save()
-#         else:
-#             Basket.objects.create(user=user_select, product=product, quantity=1)
-#
-#         products = Product.objects.all()
-#         context = {'products': products}
-#         result = render_to_string('mainapp/includes/card.html', context)
-#         return JsonResponse({'result': result})
+# def basket_add(request,id):
+#     user_select = request.user
+#     product = Product.objects.get(id=id)
+#     baskets = Basket.objects.filter(user=user_select,product=product)
+#     if baskets:
+#         basket = baskets.first()
+#         basket.quantity +=1
+#         basket.save()
+#     else:
+#         Basket.objects.create(user=user_select,product=product,quantity=1)
+#     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+@login_required
+def basket_add(request, id, id_category=None):
+    if request.is_ajax():
+        user_select = request.user
+        product = Product.objects.get(id=id)
+        baskets = Basket.objects.filter(user=user_select, product=product)
+        if baskets:
+            basket = baskets.first()
+            basket.quantity += 1
+            basket.save()
+        else:
+            Basket.objects.create(user=user_select, product=product, quantity=1)
+
+        if id_category:
+            products = Product.objects.filter(category_id=id_category).order_by('name')
+        else:
+            products = Product.objects.all().order_by('name')
+
+        context = {'products': products}
+        result = render_to_string('mainapp/includes/card.html', context)
+        return JsonResponse({'result': result})
 
 
 @login_required
--- static/js/basket.js
+++ static/js/basket.js
@@ -1,7 +1,4 @@
 window.onload = function () {
-
-
-
     $('.basket_list').on('click', 'input[type="number"]', (e) => {
         let t_href = e.target
         $.ajax(
@@ -14,18 +11,16 @@ window.onload = function () {
         e.preventDefault()
     })
 
-    var csrf = $('meta[name="csrf_token"]').attr('content');
-    $('.card_add_basket').on('click', 'button', function (event) {
-        let t_href = event.target.post.value
+    $('.card_add_basket').on('click', 'button[type="button"]', (e) => {
+        let t_href = e.target.value
         $.ajax(
             {
-                type: 'POST',
-                headers: {'X-CSRFToken': csrf},
                 url: "/baskets/add/" + t_href + "/",
                 success: function (data) {
-                    $('.basket_list').html(data.result)
+                    $('.card_add_basket').html(data.result);
+                    swal('Спасибо!', 'Ваш товар, добавлен в корзину!', 'success')
                 },
             });
-        event.preventDefault()
+        e.preventDefault()
     })
 }
