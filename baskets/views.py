cce60d1d40d09cf2f7672446bd898ead9c34f266
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2021-12-13 23:27:36 +0500

    Finally project_CBV

--- baskets/views.py
+++ baskets/views.py
@@ -1,17 +1,17 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, JsonResponse
 # Create your views here.
-from django.shortcuts import get_object_or_404
-from django.template.loader import render_to_string
-from django.views.generic import CreateView
 
+from django.template.loader import render_to_string
+from django.views.generic import CreateView, UpdateView
 from baskets.models import Basket
-from mainapp.mixin import UserDipatchMixin
+from mainapp.mixin import UserDipatchMixin, BaseClassContextMixin
 from mainapp.models import Product
 
-class BasketAddCreateView(CreateView, UserDipatchMixin):
-    title = 'GeekShop | Создать продукт'
 
+
+class BasketAddCreateView(CreateView, UserDipatchMixin, BaseClassContextMixin):
+    title = 'GeekShop | Создать продукт'
 
     def post(self, request, *args, **kwargs):
         if request.is_ajax:
@@ -31,31 +31,6 @@ class BasketAddCreateView(CreateView, UserDipatchMixin):
             return JsonResponse({'result': result})
 
 
-# @login_required
-# def basket_add(request, id):
-#     if request.is_ajax():
-#         user_select = request.user
-#         product = Product.objects.get(id=id)
-#         baskets = Basket.objects.filter(user=user_select, product=product)
-#         if baskets:
-#             basket = baskets.first()
-#             basket.quantity += 1
-#             basket.save()
-#         else:
-#             Basket.objects.create(user=user_select, product=product, quantity=1)
-#
-#         products = Product.objects.all()
-#         context = {'products': products}
-#         result = render_to_string('mainapp/includes/card.html', context)
-#         return JsonResponse({'result': result})
-
-
-@login_required
-def basket_remove(request, basket_id):
-    Basket.objects.get(id=basket_id).delete()
-    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-
-
 @login_required
 def basket_edit(request, id_basket, quantity):
     if request.is_ajax():
@@ -67,3 +42,27 @@ def basket_edit(request, id_basket, quantity):
         context = {'baskets': baskets}
         result = render_to_string('baskets/basket.html', context)
         return JsonResponse({'result': result})
+
+
+@login_required
+def basket_remove(request, basket_id):
+    Basket.objects.get(id=basket_id).delete()
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+# class BasketUpdateView(UpdateView, UserDipatchMixin):
+#     slug_url_kwarg = 'quantity'
+#     pk_url_kwarg = 'basket_id'
+#     model = Basket
+#     template_name = 'baskets/basket.html'
+#     success_url = reverse_lazy('authapp:profile')
+#
+#     def get_object(self, queryset=None):
+#         if self.request.is_ajax():
+#             basket = Basket.objects.get(id=self.kwargs.get(self.pk_url_kwarg))
+#             basket.quantity = self.kwargs.get(self.slug_url_kwarg)
+#             basket.save()
+#
+#             baskets = Basket.objects.filter(user=self.request.user)
+#             context = {'baskets': baskets}
+#             result = render_to_string('baskets/basket.html', context)
+#             return JsonResponse({'result': result}, safe=False)
--- static/js/basket.js
+++ static/js/basket.js
@@ -1,30 +1,36 @@
 window.onload = function () {
 
-
-
-    $('.basket_list').on('click', 'input[type="number"]', (e) => {
-        let t_href = e.target
+    $('.basket_list').on('click', 'input[type="number"]', function (event) {
+        let t_href = event.target
+        console.log(t_href)
         $.ajax(
             {
                 url: "/baskets/edit/" + t_href.name + "/" + t_href.value + "/",
                 success: function (data) {
                     $('.basket_list').html(data.result)
                 },
+                error: function (data){
+                    console.log(data)
+                }
             });
-        e.preventDefault()
+        event.preventDefault()
     })
 
     var csrf = $('meta[name="csrf_token"]').attr('content');
-    $('.card_add_basket').on('click', 'button', function (event) {
-        let t_href = event.target.post.value
+    $('.button_basket').on('click', 'button', function (event) {
+        let t_href = event.target.value
+        console.log(t_href)
         $.ajax(
             {
                 type: 'POST',
                 headers: {'X-CSRFToken': csrf},
                 url: "/baskets/add/" + t_href + "/",
                 success: function (data) {
-                    $('.basket_list').html(data.result)
+                    $('.card_add_basket').html(data.result)
                 },
+                error: function (data){
+                    console.log(data)
+                }
             });
         event.preventDefault()
     })
