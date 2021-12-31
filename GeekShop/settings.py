466b990ac584383a407e1c2da43a513c6858ce50
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2021-12-31 15:14:12 +0500

    Vk

--- GeekShop/settings.py
+++ GeekShop/settings.py
@@ -91,11 +91,11 @@ WSGI_APPLICATION = 'GeekShop.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'geekshop',
-        'USER': 'postgres',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'geekshop',
+        # 'USER': 'postgres',
 
     }
 }
