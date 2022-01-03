de1b9dc28552d58be69fdc7677bc134e907a9b74
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2022-01-03 15:51:10 +0500

    fixed_bags_6

--- GeekShop/settings.py
+++ GeekShop/settings.py
@@ -148,7 +148,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (BASE_DIR / 'static',)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
