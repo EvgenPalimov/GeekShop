db45cf03b8dd1576b7accd8d0dec75db78c84b6a
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2022-01-16 01:59:01 +0500

    fixed_bags 8.1

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,7 +7,7 @@ app_name = 'mainapp'
 urlpatterns = [
     path('', CatalogListView.as_view(), name='products'),
     path('category/<int:id_category>', CatalogListView.as_view(), name='category'),
-    path('page/<int:page>', cache_page(3600)(CatalogListView.as_view()), name='page'),
+    path('page/<int:page>', CatalogListView.as_view(), name='page'),
     path('detail/<int:pk>/', ProductDetail.as_view(), name='detail'),
 ]
 
