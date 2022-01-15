e934f92fdc40d28af0635212cdd36cbda0226543
Uegene Palimov <76953240+EvgenPalimov@users.noreply.github.com>
2022-01-15 14:25:50 +0500

    fixed_bags 2

--- mainapp/views.py
+++ mainapp/views.py
@@ -23,7 +23,7 @@ class CatalogListView(ListView, BaseClassContextMixin):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super(CatalogListView, self).get_context_data(**kwargs)
 
-        context['categories'] = ProductCategory.objects.all()
+        context['categories'] = get_link_category()
         if self.kwargs:
             products = Product.objects.filter(category_id=self.kwargs.get('id_category')).select_related('category')
         else:
